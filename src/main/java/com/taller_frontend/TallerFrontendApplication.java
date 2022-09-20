5224b8104c344997cb82b2ffbc3a49ca88a3ac9b
Mauricio herreram <ofmami7@gmail.com>
2022-09-20 18:04:40 -0500

    first commit

---
+++ src/main/java/com/taller_frontend/TallerFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.taller_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerFrontendApplication.class, args);
+	}
+
+}
