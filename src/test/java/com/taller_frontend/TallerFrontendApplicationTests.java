e7a019c199a370c1332dc1013626381fc065b4d4
Mauricio herreram <ofmami7@gmail.com>
2022-09-20 18:04:40 -0500

    first commit

---
+++ src/test/java/com/taller_frontend/TallerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
