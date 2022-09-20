167b2332b0abeaafce2e66e4c4c03ff097bc6b41
Mauricio herreram <ofmami7@gmail.com>
2022-09-20 18:31:32 -0500

    segundo commit

---
+++ src/main/java/com/taller_frontend/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.taller_frontend.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String index(){
+
+        return "index";
+
+    }
+}
