c346d1884af1b95f97c95d92d6041678e8fdb103
Mauricio herreram <ofmami7@gmail.com>
2022-09-20 19:55:25 -0500

    ultimo commit

--- src/main/java/com/taller_frontend/controller/IndexController.java
+++ src/main/java/com/taller_frontend/controller/IndexController.java
@@ -2,10 +2,12 @@ package com.taller_frontend.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @GetMapping
     public String index(){
 
         return "index";
